58d0d43bd533dcc7ebeabd769c551a946774aa71
ziyun <jykim306@gmail.com>
2019-04-24 16:53:03 +0900

    component 생성 및 props, state 사용

--- _0424/react-app/src/App.js
+++ _0424/react-app/src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class Subject extends Component{
+  render(){
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}  
+      </header>
+
+    );
+  }
+}
+class TOC extends Component{
+  render(){
+    var tags = [];
+    var con = this.props.data;
+    var i = 0;
+    while(i<con.length){
+      tags.push(<li key={con[i].id}><a href="">{con[i].title}</a></li>);
+      i = i + 1;
+    }
+    return (
+      <nav>
+        <ol>
+            {tags}
+        </ol>
+    </nav>
+    );
+  }
+}
+class Contents extends Component{
+  render(){
+    return (
+      <article>
+            <h2>{this.props.title}</h2>
+            {this.props.desc}  
+      </article>
+    );
+  }
+}
+class App extends Component{
+  state = {
+    contents : [
+      {id:1, title: 'HTML', desc: 'HTML is ...'},
+      {id:2, title: 'CSS', desc: 'CSS is ...'}
+    ]
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="World!"></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <TOC data={this.state.contents}></TOC>
+        <TOC data={this.state.contents}></TOC>
+        <Contents title="HTML" desc="HTML is HyperText ...!!!!"></Contents>
+      </div>
+    );
+  }
 }
 
 export default App;
